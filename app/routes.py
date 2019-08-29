aac44a022e30ebf76eaf45e56a751ee0bdcfb98d
Jatin-Nagpal <nagpaljatin1411@gmail.com>
2019-08-29 22:28:56 +0530

    Added Socket Libraries

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+import sys
+import socket
+import selectors
+import traceback
+import libserver
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return 'Hello World'
+
+@app.route('/hello/<name>/')
+def hello(name):
+	return 'Hello %s!' % name
 
 if __name__ == '__main__':
 	app.run(debug=True)
