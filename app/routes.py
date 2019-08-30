e7e05279fb1defa4a86412d0b04bf316de7c2626
Jatin-Nagpal <nagpaljatin1411@gmail.com>
2019-08-30 20:51:45 +0530

    Rerouting techniques

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template, url_for, redirect
-import sys
-import socket
-import selectors
-import traceback
-import libserver
+from flask import Flask, render_template, url_for, redirect, request
+import sys, socket, selectors, traceback
 
 app = Flask(__name__)
 
+@app.route('/success/<name>/')
+def success(name):
+	return 'welcome %s' % name
+
+@app.route('/login/',methods = ['POST','GET'])
+def login():
+	if request.method == 'POST':
+		user = request.form['nm']
+		return redirect(url_for('success',name = user))
+	else:
+		user = request.args.get('nm')
+		return redirect(url_for('success',name = user))
+
+
 @app.route('/')
 def home():
-	return 'Hello World'
+	return render_template('login.html')
+	# return redirect(url_for('login'))
 
 @app.route('/hello/<name>/')
 def hello(name):
