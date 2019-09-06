fbdd6848afc5a186433b3fbb1cdac081889cf176
Jatin-Nagpal <nagpaljatin1411@gmail.com>
2019-09-06 13:03:04 +0530

    Look Ahead in build

--- app/4_line_1_pl.cpp
+++ app/4_line_1_pl.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define f(i,x,n) for(int i=x;i<n;i++)
+#define ld long double
 
 // Let Bot is 2 and User is 1
+int filled_rows=0;
 int n=8,m=8;
 int la;
 int a[16][16],sum=0;
@@ -25,8 +27,58 @@ int check()
 		}
 	return 0;
 }
+ld dfs(int i,int la,int turn)
+{
+	ld ret=0;
+	if(sum==64)
+		return -ret;
+	if(la==0)
+		return -ret;
+	if(la==1)
+	{
+		int val=check();
+		if(val==1)
+		{
+			ret=-1;
+			return -ret;
+		}
+		else if(val==2)
+		{
+			ret=1;
+			return -ret;
+		}
+		else
+		{
+			return -ret;
+		}
+	}
+	ld div=0;
+	f(i,0,m)
+		if(b[i]>=0)
+			div++;
+	f(i,0,m)
+	{
+		if(b[i]>=0)
+		{
+			sum++;
+			a[b[i]][i]=turn;
+			b[i]--;
+			if(check()==turn)
+			{
+				ret+=1/div;
+				continue;
+			}
+			// ret+=dfs()/div;
+			b[i]++;
+			a[b[i]][i]=0;
+			sum--;
+		}
+	}
+	return -ret;
+}
 int32_t main()
 {
+	srand(time(NULL));
 	cout<<"Enter 1 if u want to play first\n";
 	int turn;
 	cin>>turn;
@@ -74,8 +126,44 @@ int32_t main()
 		}
 		else
 		{
-			vector <pair <int,int> > mov;
-			
+			ld win[m];
+			for(int i=0;i<m;i++)
+			{
+				win[i]=0;
+				if(b[i]>=0)
+				{
+					win[i]=-dfs(i,la,2);
+				}
+				else
+					win[i]=-10;
+			}
+			vector <int> mi;
+			ld minn=-11;
+			for(int i=0;i<m;i++)
+			{
+				if(minn<win[i])
+				{
+					mi.clear();
+					minn=win[i];
+					mi.push_back(i);
+				}
+				else if(minn==win[i])
+				{
+					mi.push_back(i);
+				}
+			}
+			int ch = rand()%mi.size();
+			ch=mi[ch];
+			int flag=0;
+			if(b[ch]<0)
+			{
+				cout<<"Wrong Move, the column is already filled, Enter a different move\n";
+				continue;
+			}
+			cout<<"Bot moved "<<ch<<'\n';
+			sum++;
+			a[b[ch]][ch]=turn;
+			b[ch]--;
 		}
 		turn =3-turn;
 	}
